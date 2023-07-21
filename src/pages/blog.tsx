7b88f19e98afe2700b1a2f7e6b4a8758b210436e
Peter Macdonald <macdonald.peter90@gmail.com>
2023-07-21 08:48:38 +0200

    Fixing Stuff
    Signed-off-by: Peter Macdonald <macdonald.peter90@gmail.com>

--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -21,36 +21,36 @@ const BlogIndex = ({ data, location }) => {
 
   return (
     <Layout location={location} title={siteTitle}>
-        {posts.map(post => {
-          const title = post.frontmatter.title || post.fields.slug
+      {posts.map(post => {
+        const title = post.frontmatter.title || post.fields.slug
 
-          return (
-            <li key={post.fields.slug}>
-              <article
-                className="post-list-item"
-                itemScope
-                itemType="http://schema.org/Article"
-              >
-                <header>
-                  <h2>
-                    <Link to={post.fields.slug} itemProp="url">
-                      <span itemProp="headline">{title}</span>
-                    </Link>
-                  </h2>
-                  <small>{post.frontmatter.date}</small>
-                </header>
-                <section>
-                  <p
-                    dangerouslySetInnerHTML={{
-                      __html: post.frontmatter.description || post.excerpt,
-                    }}
-                    itemProp="description"
-                  />
-                </section>
-              </article>
-            </li>
-          )
-        })}
+        return (
+          <li key={post.fields.slug}>
+            <article
+              key={post.fields.slug}
+              itemScope
+              itemType="http://schema.org/Article"
+            >
+              <header>
+                <h2>
+                  <Link to={post.fields.slug} itemProp="url">
+                    <span itemProp="headline">{title}</span>
+                  </Link>
+                </h2>
+                <small>{post.frontmatter.date}</small>
+              </header>
+              <section>
+                <p
+                  dangerouslySetInnerHTML={{
+                    __html: post.frontmatter.description || post.excerpt,
+                  }}
+                  itemProp="description"
+                />
+              </section>
+            </article>
+          </li>
+        )
+      })}
     </Layout>
   )
 }
